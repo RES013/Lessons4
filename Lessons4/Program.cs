c544a7126b8bbe5000e99aa2766dbee173bf6083
Эмир <@DESKTOP-AI8L0VK>
2024-01-18 21:04:26 +0300

    Добавьте файлы проекта.

---
+++ Lessons4/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!"
+
+
+int c = 5598486;
+int d = 13015126;
+int s1 = d - c;
+Console.Write($"В Москве больше жителей чем в Санкт-Петербурге на: {s1}");
+
+decimal wageForYear = 260548.56895m;
+Console.WriteLine($"Зарплата за год {wageForYear:F2}");
+decimal wageForMounth = wageForYear / 12;
+Console.WriteLine($"Зарплата за месяц {wageForMounth:F2}");
+decimal NDFL = wageForMounth * 13 / 100;
+Console.WriteLine($"НДФЛ {NDFL}");
+decimal wageWhitoutNDFL = wageForMounth - NDFL;
+Console.WriteLine($"Зарплата c вычетом НДФЛ {wageWhitoutNDFL:F2}");
